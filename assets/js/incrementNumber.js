f3e0eeac79bfdd1afba38cacd6b72957e11de3bf
aposkarag <aposkarag@islab.ece.ntua.gr>
2023-03-22 18:19:40 +0200

    [NR-3] strengths section implemented

---
+++ assets/js/incrementNumber.js
@@ -0,0 +1,46 @@
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function isScrolledIntoView(elem) {
+  var rect = elem.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+  
+  return isVisible;
+}
+
+
+const obj = document.getElementById("increment-number");
+let scrolled = false;
+window.addEventListener('scroll', function() {
+  setTimeout(() => {
+    if (isScrolledIntoView(obj) && !scrolled) {
+      scrolled = true;
+      animateValue(obj, 0, 80, 1000);
+      window.removeEventListener('scroll', function() {});
+    }
+  }, "1000");
+});
+
+if (isScrolledIntoView(obj) && !scrolled) {
+  setTimeout(() => {
+    if (isScrolledIntoView(obj) && !scrolled) {
+      scrolled = true;
+      animateValue(obj, 0, 80, 1000);
+      window.removeEventListener('scroll', function() {});
+    }
+  }, "1000");
+}
