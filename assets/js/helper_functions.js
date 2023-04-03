a58f282d33eb188f3d51677d19738a9ac025d9d2
aposkarag <aposkarag@islab.ece.ntua.gr>
2023-04-03 12:36:34 +0300

    [NR-3] typewriter animation and minor tweaks

--- assets/js/helper_functions.js
+++ assets/js/helper_functions.js
@@ -1,3 +1,26 @@
+function typewriter() {
+  sContents = ' ';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  let destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + '<br />';
+  }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iRow == aText.length -1){
+      destination.innerText = destination.innerText.split("_")[0];
+    }
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+}
 
 function animateValue(obj, start, end, duration) {
   let startTimestamp = null;
@@ -18,19 +41,19 @@ function isScrolledIntoView(elem) {
   var elemBottom = rect.bottom;
 
   var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-  
+
   return isVisible;
 }
 
 
 const obj = document.getElementById("increment-number");
 let scrolled = false;
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   setTimeout(() => {
     if (isScrolledIntoView(obj) && !scrolled) {
       scrolled = true;
       animateValue(obj, 0, 80, 1000);
-      window.removeEventListener('scroll', function() {});
+      window.removeEventListener('scroll', function () { });
     }
   }, "1000");
 });
@@ -40,17 +63,32 @@ if (isScrolledIntoView(obj) && !scrolled) {
     if (isScrolledIntoView(obj) && !scrolled) {
       scrolled = true;
       animateValue(obj, 0, 80, 1000);
-      window.removeEventListener('scroll', function() {});
+      window.removeEventListener('scroll', function () { });
     }
   }, "1000");
 }
 
 let directFormButtons = document.getElementsByClassName("go-to-form-btn");
 for (let btn of directFormButtons) {
-  btn.addEventListener('click', function() {
-    setTimeout( function() {
+  btn.addEventListener('click', function () {
+    setTimeout(function () {
 
       document.getElementById('message').focus();
     }, 1000)
   })
 }
+
+let aText = new Array(
+  "You dream it,",
+  "We develop IT"
+);
+let iSpeed = 100; // time delay of print out
+let iIndex = 0; // start printing array at this posision
+let iArrLength = aText[0].length; // the length of the text array
+let iScrollAt = 20; // start scrolling up at this many lines
+
+let iTextPos = 0; // initialise text position
+let sContents = ''; // initialise contents variable
+let iRow; // initialise current row
+
+setTimeout(typewriter, 1000)
